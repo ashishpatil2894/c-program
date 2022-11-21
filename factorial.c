1aed4e4492a8daa42f3593be4d9c8a3c2bbe41b2
ashishpatil2894 <118714488+ashishpatil2894@users.noreply.github.com>
2022-11-21 12:38:28 +0530

    factorial & addition of two number

--- factorial.c
+++ factorial.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+void add();
 main()
 {
 int i,fact=1,n;
@@ -9,3 +10,12 @@ for(i=0;i<=n;i++)
 fact=fact+i;
 }
 printf("\n fact=%d,fact);
+add();
+       }
+       void add()
+       {
+         int a,b,c;
+         printf("\n enter two number=");
+         scanf("%d",&a,&b);
+         c=a+b;
+         printf("\n addition of two number=%d",c);
